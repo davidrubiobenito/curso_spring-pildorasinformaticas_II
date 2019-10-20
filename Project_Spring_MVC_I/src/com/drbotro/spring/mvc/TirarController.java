6c8788dd7596ed059b1debf63d04bc044735bf76
David Rubio <davidrubiobenito@gmail.com>
2019-10-20 17:31:39 +0200

    VIDEO-33: @RequestMapping y conflicto de rutas.

--- Project_Spring_MVC_I/src/com/drbotro/spring/mvc/TirarController.java
+++ Project_Spring_MVC_I/src/com/drbotro/spring/mvc/TirarController.java
@@ -6,8 +6,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-@RequestMapping("/principal")
-public class PeticionRespuestaController{
+@RequestMapping("/secundario")
+public class TirarController{
 
     // 1. Pedir al servidor el formulario
     @RequestMapping("/peticionAlumnosFormulario")
@@ -15,18 +15,12 @@ public class PeticionRespuestaController{
         return "peticionAlumnosFormulario";
     }
 
-    // 2. Procesar el formulario que hemos rellenado y mandado
-    @RequestMapping("/procesarAlumnosFormulario")
-    public String procesarAlumnosFormulario(){
-        return "procesarAlumnosFormulario";
-    }
-
     // 2. Procesar el formulario que hemos rellenado y añadiendo datos al modelo
     @RequestMapping("/procesarModeloAlumnosFormulario")
     public String procesarModeloAlumnosFormulario(@RequestParam("nombreAlumno") String nombre, Model modelo){
         // 3. Capturar la información del formulario
-        nombre += " Es el mejor alumno.";
-        String mensajeFinal = "¿Quien es el mejor alumno? " + nombre;
+        nombre += " Es el peor alumno.";
+        String mensajeFinal = "¿Quien es el peor alumno? " + nombre;
 
         // 4. Agregamos info al modelo
         modelo.addAttribute("mensajeClaro", mensajeFinal);
