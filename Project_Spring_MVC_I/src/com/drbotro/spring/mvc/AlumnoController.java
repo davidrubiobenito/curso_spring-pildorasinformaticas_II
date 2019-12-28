ef55c4f9f07c519bc03781dd3def3e2b2c3a1e9b
David Rubio <davidrubiobenito@gmail.com>
2019-12-28 11:53:37 +0100

    VIDEO-41: Validación de formularios IV. @InitBinder

--- Project_Spring_MVC_I/src/com/drbotro/spring/mvc/AlumnoController.java
+++ Project_Spring_MVC_I/src/com/drbotro/spring/mvc/AlumnoController.java
@@ -2,15 +2,24 @@ package com.drbotro.spring.mvc;
 
 import javax.validation.Valid;
 
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/alumno")
 public class AlumnoController{
+
+    @InitBinder
+    public void miBinder(WebDataBinder binder){
+        StringTrimmerEditor recortaEspaciosEnBlanco = new StringTrimmerEditor(true);
+        binder.registerCustomEditor(String.class, recortaEspaciosEnBlanco);
+    }
 
     @RequestMapping("/registroAlumno")
     public String formularioRegistroAlumnos(Model modelo){
